9d86d510a58174d08769c593ca6de4dec6551dbc
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-24 13:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let code="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	let ans=''
+	for(let i=0;i<l;i++){
+		let substr=code[Math.floor(Math.random()*62)]
+		ans+=(substr)
+	}
+	return ans
 }
 
 // Do not change the code below.
